1c4c8a105a125eaec103b8aa254e571d92387272
Miguel-Serrano-M <97687022+Miguel-Serrano-M@users.noreply.github.com>
2022-03-03 18:04:45 +0100

    added more files

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+try:
+    age = (input("How old are you?"))
+    print("in 2032 you will be", int(age) + 10)
+    print(100/int(age))
+except ValueError:
+    print("Hey, that is not a number")
+except ZeroDivisionError:
+    print("You cannot divide by 0")
+except:
+    print("this is a new error that I did not see")
+finally:
+    print("this is what we do after all is done")
---
+++ while_example.py
@@ -0,0 +1,9 @@
+sum=0
+    while True:
+        number=int(input("give me a number (o to stop)")
+        a = a - 1
+        if number == 0:
+            break
+    sum = sum + number
+
+print("the sum is",sum)
